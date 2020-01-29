fd3aeaeb4d50b68e479bbafd01b001bb4a9e6e68
John Chapman <john.chapman@systemsresearch.linkpc.net>
2020-01-29 13:39:17 -0500

    changed from intro.R file to demo.R file to match previous RStudio.Cloud attempt

---
+++ demo.R
@@ -0,0 +1,24 @@
+# Demo file
+# 2020 Spring - BUS 355 Intro to Data Science
+
+5 + 7
+
+x <- 5 + 7
+
+x <- 21
+
+
+library(ggplot2)
+
+mpg
+
+ggplot(data = mpg) +
+  geom_point(mapping = aes(x = hwy, y = cty))
+
+ggplot(data = mpg) +
+  geom_point(mapping = aes(x = hwy, y = displ))
+
+
+
+
+
--- intro.R
+++
@@ -1,2 +0,0 @@
-# BUS 355 Intro to Data Science - 2020 Spring
-# Introduction file
