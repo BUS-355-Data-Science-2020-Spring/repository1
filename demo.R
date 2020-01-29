13df8e398b485cf869c721a1b5b26532d98357d6
John Chapman <john.chapman@systemsresearch.linkpc.net>
2020-01-29 13:40:25 -0500

    update comments

--- demo.R
+++ demo.R
@@ -7,14 +7,21 @@ x <- 5 + 7
 
 x <- 21
 
-
+# this next line loads the ggplot2 package
 library(ggplot2)
 
+# this next line allows you to see the mpg data, which comes from the package
 mpg
 
+
+# the next 2 lines is the command to create a graph of the mpg data; and looks at the 
+# relationship between hwy (which is miles per gallon on the highway) and cty (which 
+# is miles per gallon in the city)
 ggplot(data = mpg) +
   geom_point(mapping = aes(x = hwy, y = cty))
 
+
+# this next line looks at hwy and displ (which is engine size--or displacement)
 ggplot(data = mpg) +
   geom_point(mapping = aes(x = hwy, y = displ))
 
